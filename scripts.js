93b60e4d2a242dc2ae12a00d06e2b9c59218a6d9
soap2k6 <pdsoper@gmail.com>
2015-07-27 16:02:50 -0400

    Logic complete. Need to complete CSS on 'case'

--- scripts.js
+++ scripts.js
@@ -9,124 +9,114 @@ $( document ).ready(function() {
     var stack = [];
 
     var operations = {
-        plus: function(a,b) { return a + b; },
-        minus: function(a,b) { return a - b; },
-        times: function(a,b) { return a * b; },
+        plus:   function(a,b) { return a + b; },
+        minus:  function(a,b) { return a - b; },
+        times:  function(a,b) { return a * b; },
         divide: function(a,b) { return (b === 0) ? "Error" : a / b; },
-        pct: function(a,b) { return a * b / 100; },
-    }
+        pct:    function(a,b) { return a * b / 100; },
+    };
 
-    function updateDisplay(val) {
-        display = val;
-        $( '#display' ).html('').html(display);
-    }
+    /* Calculator logic */
 
-    function appendToDisplay(val) {
-        var currentVal = $.trim($( '#display' ).text());
-        if (currentVal.length >= maxLength) {
+    /*
+    √ CE clears the current entry without changing the stored value or operator
+    √ AC clears everything
+    √ Pressing a number
+        adds the digit to the displayed value in input mode
+        starts a new value if the last key was an operator or equal or AV or CE
+            This is determined by readyForNext
+    √ Pressing .
+        does nothing in input mode if the numnber already contains a .
+        otherwise, it acts like a digit.
+    Pressing an operator key
+        If the previous pressed key was an oerator, it replaces it
+        If there is a pending operation, it performs it
+        it stores the current value, and the name of the operator
+    Pressing the = keuy
+        Evaluates the pending operation, if any
+        If pressed right after an operator, it clears that operator.
+        If the number in the display has not been editted, still use it in the evaluations.
+    */
+
+    function evalStack(val) {
+        readyForNext = true;
+        hasDot = false;
+        if (!readyForEval()) {
+            stack = [val];
+            updateDisplay(trimToMax(val));
             return;
         }
-        console.log(currentVal, val);
-        if (currentVal === '0' && val !== '.' && hasDot === false) {
-            updateDisplay(val);
-        } else {
-            updateDisplay(currentVal + val.toString());
-        }
-    }
-
-    function trimToMax(val) {
-        if (val.toString().length <= maxLength) {
-            return val;
-        }
-        console.log(val);
-        var parts = val.toString().split('.');
-        console.log(parts);
-        if (parts[0].length > maxLength) {
-            return "Error";
-        } else {
-            return myRound(Number(val), maxLength - parts[0].length);
-        }
-    }
-
-    function myRound(num, decimals) {
-        var factor = Math.pow(10, decimals);
-        return Math.round(factor * num) / factor;
-    }
-
-    function evalStack() {
-        if (stack.length < 3) {
-            return;
-        }
-        console.log(stack);
-        var arg1 = Number(stack.shift());
-        var op = stack.shift();
-        var arg2 = Number(stack.shift());
+        var arg1 = Number(stack[0]);
+        var op = stack[1];
+        var arg2 = Number(val);
         var ans = operations[op](arg1, arg2);
-        stack.unshift(ans);
+        stack = [ans];
         updateDisplay(trimToMax(ans));
     }
 
+    function readyForEval() {
+        if (stack.length < 2) {
+            return false;
+        }
+        return ( !isNaN(Number(stack[0])) &&
+            Object.getOwnPropertyNames(operations).indexOf(stack[1]) > -1 );
+    }
+
+    function replaceOperator(val) {
+        if (readyForNext && stack.length === 2) {
+            stack[1] = val;
+            return true;
+        }
+        return false;
+    }
+
+    /* Event handlers */
+
     $( '#key-plus' ).click(function()  {
-        stack.push($( '#display' ).text());
-        evalStack();
+        if (replaceOperator('plus')) {
+            return;
+        }
+        evalStack($( '#display' ).text());
         stack.push('plus');
-        readyForNext = true;
     });
 
     $( '#key-minus' ).click(function()  {
-        stack.push($( '#display' ).text());
-        evalStack();
+        if (replaceOperator('minus')) {
+            return;
+        }
+        evalStack($( '#display' ).text());
         stack.push('minus');
-        readyForNext = true;
     });
 
     $( '#key-times' ).click(function()  {
-        stack.push($( '#display' ).text());
-        evalStack();
+        if (replaceOperator('times')) {
+            return;
+        }
+        evalStack($( '#display' ).text());
         stack.push('times');
-        readyForNext = true;
     });
 
     $( '#key-divide' ).click(function()  {
-        stack.push($( '#display' ).text());
-        evalStack();
+        if (replaceOperator('divide')) {
+            return;
+        }
+        evalStack($( '#display' ).text());
         stack.push('divide');
-        readyForNext = true;
     });
 
     $( '#key-pct' ).click(function()  {
-        stack.push($( '#display' ).text());
-        evalStack();
+        if (replaceOperator('pct')) {
+            return;
+        }
+        evalStack($( '#display' ).text());
         stack.push('pct');
-        readyForNext = true;
     });
 
     $( '#key-equal' ).click(function()  {
-        stack.push($( '#display' ).text());
-        evalStack();
-        readyForNext = true;
-    });
-
-    $( '#key-ac' ).click(function()  {
-        hasDot = false;
-        stack = [];
-        updateDisplay(0);
-    });
-
-    $( '#key-ce' ).click(function()  {
-        hasDot = false;
-        updateDisplay(0);
-    });
-
-    function numeral(val) {
-        if (readyForNext) {
-            hasDot = false;
-            updateDisplay(val);
-            readyForNext = false;
-        } else {
-            appendToDisplay(val);
+        if (stack.length === 2) {
+            evalStack($( '#display' ).text());
         }
-    }
+    });
 
     $( '#key-0' ).click(function() { numeral(0); });
     $( '#key-1' ).click(function() { numeral(1); });
@@ -139,12 +129,76 @@ $( document ).ready(function() {
     $( '#key-8' ).click(function() { numeral(8); });
     $( '#key-9' ).click(function() { numeral(9); });
 
+    function numeral(val) {
+        if (readyForNext) {
+            updateDisplay(val);
+            readyForNext = false;
+        } else {
+            appendToDisplay(val);
+        }
+    }
+
     $( '#key-dot' ).click(function()  {
         if (hasDot) {
             return;
         }
         hasDot = true;
-        appendToDisplay('.');
+        if (readyForNext) {
+            updateDisplay('0.');
+            readyForNext = false;
+        } else {
+            appendToDisplay('.');
+        }
     });
+
+    $( '#key-ac' ).click(function()  {
+        stack = [];
+        hasDot = false;
+        readyForNext = true;
+        updateDisplay(0);
+    });
+
+    $( '#key-ce' ).click(function()  {
+        hasDot = false;
+        readyForNext = true;
+        updateDisplay(0);
+    });
+
+   /* Writing values to the calculator's "display" */
+
+    function updateDisplay(val) {
+        display = val;
+        $( '#display' ).html('').html(display);
+    }
+
+    function appendToDisplay(val) {
+        var currentVal = $.trim($( '#display' ).text());
+        if (currentVal.length >= maxLength) {
+            return;
+        }
+        if (currentVal === '0' && val !== '.' && hasDot === false) {
+            updateDisplay(val);
+        } else {
+            updateDisplay(currentVal + val.toString());
+        }
+    }
+
+    function trimToMax(val) {
+        if (val.toString().length <= maxLength) {
+            return val;
+        }
+        var parts = val.toString().split('.');
+        if (parts[0].length > maxLength) {
+            return "Error";
+        } else {
+            return myRound(Number(val), maxLength - parts[0].length);
+        }
+    }
+
+    function myRound(num, decimals) {
+        var factor = Math.pow(10, decimals);
+        return Math.round(factor * num) / factor;
+    }
+
 
 });
